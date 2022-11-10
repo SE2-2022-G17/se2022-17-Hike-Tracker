479560119076162135cd6762d275b437769011f1
SofiaBelloni <sofiabelloni99@gmail.com>
2022-11-10 15:42:16 +0100

    Added bcryptjs and pw hash

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
 const { Schema } = mongoose;
 const Type = require("./UserType")
 
@@ -25,6 +26,35 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 })
+
+//hash password before saving user
+userSchema.pre("save", function (next) {
+    const user = this;
+
+    if (this.isModified("password") || this.isNew) {
+        bcrypt.genSalt(10, function (saltError, salt) {
+            if (saltError) return next(saltError);
+            else {
+                bcrypt.hash(user.password, salt, function (err, hash) {
+                    if (err) return next(err);
+
+                    user.password = hash;
+                    next();
+                })
+            }
+        })
+    } else {
+        return next();
+    }
+});
+
+//check password
+userSchema.methods.comparePassword = function (password, callback) {
+    bcrypt.compare(password, this.password, function (error, isMatch) {
+        if (error) return callback(error);
+        callback(null, isMatch);
+    })
+}
 
 // create a model for user schema
 // it will create a user collection in the mongo database
