945b97cd306d5513c914df71f12429ee3a896e9f
fede1997483 <65650856+fede1997483@users.noreply.github.com>
2022-11-12 20:09:11 +0100

    Validation implemented

--- client/src/components/SignUpComponents.js
+++ client/src/components/SignUpComponents.js
@@ -3,6 +3,7 @@ import { Form, Button, Container, Row, Col } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { BackButton } from './Utility';
+import { VerCard } from './Verification.js'
 
 function SignUpForm(props) {
 
@@ -12,7 +13,10 @@ function SignUpForm(props) {
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [type, setType] = useState('hiker');
-
+    const [verificationVisible,setVerificationVisibile] = useState(false);
+    const [emailSent,setEmailSent] = useState(false);
+    const [signedUp,setSignedUp] = useState(false);
+    const [emailIsValidated,setEmailIsValidated] = useState(false);
     const navigate = useNavigate();
 
     const handleSubmit = (event) => {
@@ -39,7 +43,10 @@ function SignUpForm(props) {
             props.setErrorMessage('Please confirm your password correctly');
         }
         if (valid) {
+            setVerificationVisibile(true);
             props.signup(credentials);
+            setSignedUp(true);
+            setTimeout(()=>setEmailSent(true),2000);
         }
     };
 
@@ -81,10 +88,29 @@ function SignUpForm(props) {
                             <Form.Control type='password' value={confirmPassword} onChange={ev => setConfirmPassword(ev.target.value)} />
                         </Form.Group>
                         <Col className='text-center my-3'>
-                            <Button type='submit' className='primary' onClick={handleSubmit}>Sign Up</Button>
-                            <p className='alternative'>Already registered? <Link to='/login'>Login</Link></p>
+                            {
+                                signedUp ?<></>: 
+                                <>
+                                    <Button type='submit' className='primary' onClick={handleSubmit}>Sign Up</Button>
+                                </>
+                                
+                            }
+                            {
+                                emailIsValidated ? 
+                                    <Button type='submit' className='primary' onClick={()=>{navigate('/login')}}>Go to login</Button>:
+                                <></>  
+                            }
                         </Col>
                     </Form>
+                </Col>
+            </Row>
+            <Row className="justify-content-center">
+                <Col xs={6} className='text-center my-3'>
+                {
+                    verificationVisible ? 
+                    <VerCard emailSent={emailSent} email={username} setEmailIsValidated={setEmailIsValidated}/>:<></>
+                }
+                <p className='alternative'>Already registered? <Link to='/login'>Login</Link></p>
                 </Col>
             </Row>
         </Container>
---
+++ client/src/components/Verification.js
@@ -0,0 +1,94 @@
+import { Card,Form, Button, Container, Row, Col } from 'react-bootstrap';
+import React, { useState } from 'react'
+import Spinner from 'react-bootstrap/Spinner';
+import Alert from 'react-bootstrap/Alert';
+import API from '../API';
+
+function VerCard(props){
+    const [code,setCode] = useState('Insert code');
+    const [valid,setValid] = useState(false);
+    const [messageVisible,setMessageVisible] = useState(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // validation that forms are not empty
+        
+        if (code == "") {
+            setValid(false);
+            setMessageVisible(true);
+        }
+        else{
+            setValid(true);
+            API.validateEmail(props.email,code)
+            .then((response)=>{
+                if(response=='OK'){
+                    props.setEmailIsValidated(true);
+                    setMessageVisible(true);
+                    setValid(true);
+                    console.log("True");
+                }
+                else{
+                    props.setEmailIsValidated(false);
+                    setMessageVisible(true);
+                    setValid(false);
+                    console.log("False");
+                };
+            })
+        }
+    }
+
+    return(<>
+        <Card>
+            <Card.Body>
+                <Form>
+                    <Container>
+                        {
+                            props.emailSent ?
+                                <Row>
+                                    <Col>
+                                        <Card.Text>
+                                            Verification code sent on email.
+                                     </Card.Text>
+                                    </Col>
+                                </Row>:
+                                <></>
+                        }
+                        <Row>
+                            <Col>
+                                <Form.Group controlId='verifyCode' className='base-form'>
+                                    <Form.Control type='text' value={code} onClick={()=>setCode("")}
+                                    onChange={ev => setCode(ev.target.value)} />
+                                </Form.Group>
+                            </Col>
+                            <Col>
+                                {
+                                    props.emailSent ? 
+                                    <Button type='submit' onClick={handleSubmit} className='primary'>Verify</Button>:
+                                    <Spinner animation="border" variant="success" />
+                                }
+                            </Col>
+                        </Row>
+                        {
+                            messageVisible ?
+                                valid ?
+                                    <Row>
+                                        <Col>
+                                            <Alert variant='success'>Email correctly verified.</Alert>
+                                        </Col>
+                                    </Row> :
+                                    <Row>
+                                    <Col>
+                                        <Alert variant='danger'>Uncorrect code.</Alert>
+                                    </Col>
+                                </Row>
+                                :<></>
+                        }
+                    </Container>
+                </Form>
+            </Card.Body>
+        </Card>
+    </>);
+}
+
+export {VerCard} ;
