befeb9f973d00e087f71e9e3cb1625cc7cdd8816
andreaamato-polito <s292418@studenti.polito.it>
2022-11-13 14:39:11 +0100

    fix: add auth token to tests

--- server/tests/userStory2.test.js
+++ server/tests/userStory2.test.js
@@ -1,11 +1,12 @@
 const request = require('supertest');
 const app = require("../server.js");
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken')
 
 mongoose.connect("mongodb://localhost/hike_tracker");
 
 describe('Test API for creating hikes (US2)', () => {
-    it('test create correct hike', async () => {
+    it('test create hike - unauthorized', async () => {
 
         const response = await request(app)
             .post("/localGuide/addHike")
@@ -25,7 +26,37 @@ describe('Test API for creating hikes (US2)', () => {
             });
 
 
-        expect(response.statusCode).toBe(201);
+        expect(response.statusCode).toBe(401);
+    })
+
+
+    it('test create correct hike', async () => {
+        const token = jwt.sign({
+            'email': "test@email.com",
+            'role': "localGuide",
+            'active': "true"
+        }, 'my_secret_key')
+
+        const response = await request(app)
+            .post("/localGuide/addHike")
+            .set('Authorization', token)
+            .send({
+                "title": "title99",
+                "length": "12",
+                "time": "45",
+                "ascent": "321",
+                "difficulty": "Tourist",
+                "startPoint": { "longitude": "37", "latitude": "13" },
+                "endPoint": { "longitude": "37", "latitude": "13" },
+                "referencePoints": [],
+                "description": "descr",
+                "track": "",
+                "city": "city99",
+                "province": "PR99"
+            });
+
+
+        expect(response.statusCode).toBe(401);
     })
 });
 
