441069a9b12b9e86d5c63a4ceb14d8724f5e106c
FrancescoSantoro7 <79816295+FrancescoSantoro7@users.noreply.github.com>
2022-11-27 21:49:13 +0100

    Update userStory8.test.js

--- server/tests/userStory8.test.js
+++ server/tests/userStory8.test.js
@@ -5,12 +5,81 @@ const jwt = require('jsonwebtoken');
 const Hike = require('../models/Hike.js');
 const Hut = require('../models/Hut.js');
 const Parking = require('../models/Parking.js');
+const Position = require('../models/Position.js');
 
 const { response } = require('express');
+const Difficulty = require('../models/Difficulty.js');
+const { ObjectId } = require('mongodb');
 
 mongoose.connect("mongodb://localhost/hike_tracker");
 
+
+beforeAll(async () => {
+
+    await Parking.deleteOne({ _id: '0000000194e4c1e796231d9a' });
+    await Hut.deleteOne({ _id: '0000000194e4c1e796231d9b'});
+    await Hike.deleteOne({ _id: '0000000194e4c1e796231d9a'});
+
+    const startPosition = await Position.create({
+        "location.coordinates": [3, 5]
+    })
+
+    const endPosition = await Position.create({
+        "location.coordinates": [4, 6]
+    })
+
+    const parking = await Parking.create(  
+        {
+        _id: new mongoose.Types.ObjectId('0000000194e4c1e796231d9a'),
+        name:"parking",
+        description:"parking test",
+        parkingSpaces:"3",
+        point: startPosition._id
+        }
+    )
+
+    const hike = await Hike.create({
+        _id: new mongoose.Types.ObjectId('0000000194e4c1e796231d9a'),
+        title: 'prova',
+        expectedTime: 20,
+        difficulty: Difficulty.Hiker,
+        city: 'Torino',
+        province: 'Torino',
+        description: 'test',
+        track_file: '..\public\tracks\Appalachian Trail.gpx',
+        length: 2,
+        ascent: 5,
+        startPoint: startPosition._id,
+        endPoint: endPosition._id
+    });
+
+    const hut = await Hut.create(
+        {
+            _id: new mongoose.Types.ObjectId('0000000194e4c1e796231d9b'),
+            beds: 6,
+            altitude: 40,
+            city: 'Torino',
+            province:'Torino',
+            point: startPosition._id,
+            name: "hut",
+            description: 'hut test'
+
+        }
+    )
+
+
+    await parking.save();
+    await hike.save();
+    await hut.save();
+
+    
+});
+
+
 describe('Test API for adding huts or parking as startPoint/arrivals  ', () => {
+
+
+    
     it('test add hut as start point', async () => {
         const token = jwt.sign({
             'fullName': "Pietro Bertorelle",
@@ -19,14 +88,9 @@ describe('Test API for adding huts or parking as startPoint/arrivals  ', () => {
             'active': true
         }, 'my_secret_key')
 
-        const hike = await Hike.findOne({}, { _id: 1 })
-        .catch(err => {
-            console.log(err);
-        });
 
-        const parkHut = await Hut.findOne({}, {_id: 1}).catch(err => {
-            console.log(err);
-        });
+        const hike = await Hike.findById( new mongoose.Types.ObjectId('0000000194e4c1e796231d9a'));
+        const parkHut = await Hut.findById(new mongoose.Types.ObjectId('0000000194e4c1e796231d9b'));
 
 
         const response = await request(app)
@@ -36,9 +100,8 @@ describe('Test API for adding huts or parking as startPoint/arrivals  ', () => {
             point: "start",
             reference: "huts",
             id: parkHut._id,
-            hikeId: hike.id
+            hikeId: hike._id
         });
-
         expect(response.statusCode).toBe(201);
     })
 
@@ -50,14 +113,8 @@ describe('Test API for adding huts or parking as startPoint/arrivals  ', () => {
             'active': true
         }, 'my_secret_key')
 
-        const hike = await Hike.findOne({}, { _id: 1 })
-        .catch(err => {
-            console.log(err);
-        });
-
-        const parkHut = await Hut.findOne({}, {_id: 1}).catch(err => {
-            console.log(err);
-        });
+        const hike = await Hike.findById( new mongoose.Types.ObjectId('0000000194e4c1e796231d9a'));
+        const parkHut = await Hut.findById(new mongoose.Types.ObjectId('0000000194e4c1e796231d9b'));
 
 
         const response = await request(app)
@@ -69,7 +126,6 @@ describe('Test API for adding huts or parking as startPoint/arrivals  ', () => {
             id: parkHut._id,
             hikeId: hike.id
         });
-
         expect(response.statusCode).toBe(201);
     })
 
@@ -81,15 +137,8 @@ describe('Test API for adding huts or parking as startPoint/arrivals  ', () => {
             'active': true
         }, 'my_secret_key')
 
-        const hike = await Hike.findOne({}, { _id: 1 })
-        .catch(err => {
-            console.log(err);
-        });
-
-        const parkHut = await Parking.findOne({}, {_id: 1}).catch(err => {
-            console.log(err);
-        });
-
+        const hike = await Hike.findById( new mongoose.Types.ObjectId('0000000194e4c1e796231d9a'));
+        const parkHut = await Hut.findById(new mongoose.Types.ObjectId('0000000194e4c1e796231d9b'));
 
         const response = await request(app)
         .put("/linkStartArrival")
@@ -100,7 +149,6 @@ describe('Test API for adding huts or parking as startPoint/arrivals  ', () => {
             id: parkHut._id,
             hikeId: hike.id
         });
-
         expect(response.statusCode).toBe(201);
     })
 
@@ -112,14 +160,8 @@ describe('Test API for adding huts or parking as startPoint/arrivals  ', () => {
             'active': true
         }, 'my_secret_key')
 
-        const hike = await Hike.findOne({}, { _id: 1 })
-        .catch(err => {
-            console.log(err);
-        });
-
-        const parkHut = await Parking.findOne({}, {_id: 1}).catch(err => {
-            console.log(err);
-        });
+        const hike = await Hike.findById( new mongoose.Types.ObjectId('0000000194e4c1e796231d9a'));
+        const parkHut = await Hut.findById(new mongoose.Types.ObjectId('0000000194e4c1e796231d9b'));
 
 
         const response = await request(app)
@@ -131,7 +173,7 @@ describe('Test API for adding huts or parking as startPoint/arrivals  ', () => {
             id: parkHut._id,
             hikeId: hike.id
         });
-
+    
         expect(response.statusCode).toBe(201);
     })
 });
