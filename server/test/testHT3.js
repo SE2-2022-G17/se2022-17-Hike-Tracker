7bc3a12d3a5a10fc40d5686af2179fab86b88c60
andreaamato-polito <s292418@studenti.polito.it>
2022-12-02 17:11:17 +0100

    test: exclude tests from duplication, add ht3

---
+++ server/test/testHT3.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const request = require('supertest');
+const app = require("../server.js");
+var chai = require('chai');
+var expect = chai.expect;
+const Type = require('../models/UserType.js');
+
+
+let mongoServer;
+
+before(async () => {
+    // if readyState is 0, mongoose is not connected
+    if (mongoose.connection.readyState === 0) {
+        mongoServer = await MongoMemoryServer.create();
+        const mongoUri = mongoServer.getUri();
+        await mongoose.connect(mongoUri);
+    }
+});
+
+after(async () => {
+    await mongoose.disconnect();
+    if (mongoServer !== undefined)
+        await mongoServer.stop();
+    app.close();
+});
+
+describe('...', () => {
+    it('test visitor registration', async () => {
+        const response = await request(app).post("/user/register")
+            .send({
+                firstName: "FirstNameTest",
+                lastName: "LastNameTest",
+                email: "s2g172022@gmail.com",
+                password: "password",
+                role: Type.hiker
+            });
+        expect(response.statusCode).to.equal(201);
+    });
+
+    it('test visitor registration with unvalid first name', async () => {
+        const response = await request(app).post("/user/register")
+            .send({
+                firstName: "",
+                lastName: "LastNameTest",
+                email: "s2g172022@gmail.com",
+                password: "password",
+                role: Type.localGuide
+            });
+        expect(response.statusCode).to.equal(400); // Error code to be defined
+    });
+
+    it('test visitor registration with unvalid last name', async () => {
+        const response = await request(app).post("/user/register")
+            .send({
+                firstName: "FirstNameTest",
+                lastName: "",
+                email: "s2g172022@gmail.com",
+                password: "password",
+                role: Type.platformManager
+            });
+        expect(response.statusCode).to.equal(400); // Error code to be defined
+    });
+
+    it('test visitor registration with unvalid email', async () => {
+        const response = await request(app).post("/user/register")
+            .send({
+                firstName: "FirstNameTest",
+                lastName: "LastNameTest",
+                email: "",
+                password: "password",
+                role: Type.hutWorker
+            });
+        expect(response.statusCode).to.equal(400); // Error code to be defined
+    });
+});
